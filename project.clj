2cdbb78a2be553eb82cbfeb5987edd2b3bac9919
Daniel Ribeiro <danrbr@gmail.com>
2013-11-12 23:19:32 -0800

    added sample project.clj

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject LightTable-Kibit "0.1.0"
+  :description "Idiomatic Clojure with LightTable"
+  :url "https://github.com/danielribeiro/LightTableKibit"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/mit-license.php"
+            :distribution :repo}
+  :warn-on-reflection false
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [lein-kibit "0.0.8"]
+                 ]
+
+
+  )
