54908e2e320705d1f7d5c1c68fbb1872ba60c3cd
Daniel Ribeiro <danrbr@gmail.com>
2013-11-18 12:12:40 -0800

    Now it does not change the selected block if it does not imrpove the code
    Thanks to @ibdknox https://groups.google.com/d/msg/light-table-discussion/abWSd721HmI/ilyoWKXmdXcJ

--- user.keymap.clj
+++ user.keymap.clj
@@ -1,10 +1,10 @@
 {:+ {
      :editor.clj {"pmeta-i" [(:eval.custom "(do (require 'kibit.check)
-                                           (if-let [better (kibit.check/check-expr '__SELECTION__)] 
-                                             (:alt better)
-                                             '__SELECTION__
-                                           ))"
-                                           {:result-type :replace})]}
+                                         (if-let [better (kibit.check/check-expr '__SELECTION__)] 
+                                         (with-out-str (clojure.pprint/write (:alt better) :suppress-namespaces true :dispatch clojure.pprint/code-dispatch))
+                                         __SELECTION*__
+                                         ))"
+                                         {:result-type :replace :verbatim true})]}
 
      }
 
