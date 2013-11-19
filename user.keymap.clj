fa0b2807f0790b0ce4fd758e80a54abbc60059cc
Daniel Ribeiro <danrbr@gmail.com>
2013-11-18 22:44:28 -0800

    reformating usermap

--- user.keymap.clj
+++ user.keymap.clj
@@ -1,11 +1,12 @@
 {:+ {
-     :editor.clj {"pmeta-i" [(:eval.custom "(do (require 'kibit.check)
-                                         (if-let [better (kibit.check/check-expr '__SELECTION__)] 
-                                         (with-out-str (clojure.pprint/write (:alt better) :suppress-namespaces true :dispatch clojure.pprint/code-dispatch))
-                                         __SELECTION*__
-                                         ))"
-                                         {:result-type :replace :verbatim true})]}
-
-     }
+  :editor.clj {"pmeta-i" [(:eval.custom
+  "(do (require 'kibit.check)
+    (if-let [better (kibit.check/check-expr '__SELECTION__)]
+    (with-out-str
+      (clojure.pprint/write (:alt better)
+        :suppress-namespaces true
+        :dispatch clojure.pprint/code-dispatch))
+    __SELECTION*__))"
+    {:result-type :replace :verbatim true})]}}
 
  }
