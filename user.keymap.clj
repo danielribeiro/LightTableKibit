2e308dbcec1824f8394bad3e6a5ecbebfe53939c
Daniel Ribeiro <danrbr@gmail.com>
2013-11-18 22:51:33 -0800

    more idiomatic indenting

--- user.keymap.clj
+++ user.keymap.clj
@@ -1,12 +1,13 @@
 {:+ {
   :editor.clj {"pmeta-i" [(:eval.custom
-  "(do (require 'kibit.check)
+  "(do
+    (require 'kibit.check)
     (if-let [better (kibit.check/check-expr '__SELECTION__)]
-    (with-out-str
-      (clojure.pprint/write (:alt better)
-        :suppress-namespaces true
-        :dispatch clojure.pprint/code-dispatch))
-    __SELECTION*__))"
+      (with-out-str
+        (clojure.pprint/write (:alt better)
+          :suppress-namespaces true
+          :dispatch clojure.pprint/code-dispatch))
+      __SELECTION*__))"
     {:result-type :replace :verbatim true})]}}
 
  }
